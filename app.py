d943fec71e158b9596b0debc2626a1021063d0eb
Samuel Lopes <samuelvitorprofissional@gmail.com>
2022-01-16 17:27:29 -0300

    :

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ app.config["DEBUG"]= True
 
 @app.route("/",methods=["GET"])
 def index():
-    data = requests.get("https://randomuser.me/api")
+    data = requests.get('https://randomuser.me/api')
     return data.json()
 
-if __name__ == "__main__"
+if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True, port="5000")
